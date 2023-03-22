232d53a9679b1a6d38a6e0bd20e9ecc99f92ce79
OnpointSoftwares <vincentbettoh@gmail.com>
2023-03-23 00:26:08 +0300

    New updates on switch

--- app/src/main/java/com/example/smartfridgecontroller/Switch.kt
+++ app/src/main/java/com/example/smartfridgecontroller/Switch.kt
@@ -4,8 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Switch : AppCompatActivity() {
+ val btSocket:BluetoothSocket ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_switch)
+
+    private void turnOffFridge()
+    {
+        if (btSocket!=null)
+        {
+            try
+            {
+                btSocket.getOutputStream().write("0".toString().getBytes());
+            }
+            catch (IOException e)
+            {
+                msg("Error");
+            }
+        }
+    }
+
+    private void turnOnFridge()
+    {
+        if (btSocket!=null)
+        {
+            try
+            {
+                btSocket.getOutputStream().write("1".toString().getBytes());
+            }
+            catch (IOException e)
+            {
+                msg("Error");
+            }
+        }
+    }
     }
 }
