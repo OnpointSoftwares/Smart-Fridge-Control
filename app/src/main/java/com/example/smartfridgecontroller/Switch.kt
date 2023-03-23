ba8050b52682e7ee030f0e96c520c84b23f65c0b
OnpointSoftwares <vincentbettoh@gmail.com>
2023-03-23 08:32:44 +0300

    Changes

--- app/src/main/java/com/example/smartfridgecontroller/Switch.kt
+++ app/src/main/java/com/example/smartfridgecontroller/Switch.kt
@@ -1,42 +1,59 @@
 package com.example.smartfridgecontroller
 
+import android.bluetooth.BluetoothSocket
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.io.IOException
+import java.io.OutputStream
 
 class Switch : AppCompatActivity() {
- val btSocket:BluetoothSocket ?= null
+ val btSocket: BluetoothSocket?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_switch)
+        val btnSwitchOff:Button=findViewById(R.id.btnSwitchOff)
+        val btnSwitchOn:Button=findViewById(R.id.btnSwitch)
+        btnSwitchOff.setOnClickListener {
+            turnOffFridge()
+        }
+        btnSwitchOn.setOnClickListener {
+            turnOnFridge()
+        }
 
-    private void turnOffFridge()
+    }
+    fun turnOffFridge()
     {
         if (btSocket!=null)
         {
             try
             {
-                btSocket.getOutputStream().write("0".toString().getBytes());
+                btSocket.getOutputStream()?.write("0".toString().toByte())
             }
-            catch (IOException e)
+            catch (e:IOException)
             {
-                msg("Error");
+                Toast.makeText(this,"Error",Toast.LENGTH_LONG).show()
             }
         }
     }
 
-    private void turnOnFridge()
+    fun turnOnFridge()
     {
         if (btSocket!=null)
         {
             try
             {
-                btSocket.getOutputStream().write("1".toString().getBytes());
+                btSocket.getOutputStream()?.write("1".toString().toByte())
             }
-            catch (IOException e)
+            catch (e:IOException)
             {
-                msg("Error");
+                Toast.makeText(this,"Error",Toast.LENGTH_LONG).show()
             }
         }
     }
-    }
+}
+
+private fun OutputStream.write(toByte: Byte) {
+
 }
